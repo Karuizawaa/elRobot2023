36bef6f488a9f6c8febfadcc71fecfa8f267696e
Karuizawa <aldirobyy@gmail.com>
2023-03-18 17:39:14 +0700

    Base Elephant

---
+++ Arduino/elephantBase/elephantBase.ino
@@ -0,0 +1,463 @@
+/*****************************************************************************
+   Saturday, 11 Feb 2023
+
+   Koding base Robot Gajah
+   Odometry Absolute
+   PID Posisi Absolute
+   rosserial
+
+   maintainer : Aldi Roby Syafriansyah
+   email      : aldirobyy@gmail.com / aldisyafriansyah@protonmail.com
+
+*****************************************************************************/
+#include "ros.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int16.h"
+#include "std_msgs/UInt8.h"
+
+bool kiri, kanan, bawah, atas, buttonSq, buttonX, buttonO, buttonTr, buttonTouch, l1, r1;
+float leftStickY, leftStickX, rightStickY, rightStickX, l2, r2;
+int8_t vX, vY;
+uint8_t caseRobot;
+void petak(const std_msgs::Bool& btpetak) {
+  buttonSq = btpetak.data;
+}
+void kali(const std_msgs::Bool& btkali) {
+  buttonX = btkali.data;
+  if (buttonX) {
+    //    Serial2.write('a');
+  }
+}
+void bulet(const std_msgs::Bool& btbulet) {
+  buttonO = btbulet.data;
+  if (buttonO) {
+    //    Serial2.write('b');
+  }
+}
+void lupis(const std_msgs::Bool& btlupis) {
+  buttonTr = btlupis.data;
+}
+void lsat(const std_msgs::Bool& btlsat) {
+  l1 = btlsat.data;
+}
+void rsat(const std_msgs::Bool& btrsat) {
+  r1 = btrsat.data;
+}
+void left(const std_msgs::Bool& btarrow) {
+  kiri = btarrow.data;
+  vX = kiri * -3;
+}
+void right(const std_msgs::Bool& btarrow) {
+  kanan = btarrow.data;
+  vX = kanan * 3;
+}
+void up(const std_msgs::Bool& btarrow) {
+  atas = btarrow.data;
+  vY = atas * 3;
+}
+void down(const std_msgs::Bool& btarrow) {
+  bawah = btarrow.data;
+  vY = bawah * -3;
+}
+void rdua(const std_msgs::Float32& trigg) {
+  r2 = trigg.data;
+  r2 = -(r2 - 1) * 1;
+}
+void ldua(const std_msgs::Float32& trigg) {
+  l2 = trigg.data;
+  l2 = -(l2 - 1) * 1;
+}
+void lx(const std_msgs::Float32& trigg) {
+  leftStickX = trigg.data;
+  leftStickX = -leftStickX * 3;
+}
+void ly(const std_msgs::Float32& trigg) {
+  leftStickY = trigg.data;
+  leftStickY = leftStickY * 3;
+}
+void recCase(const std_msgs::UInt8& caseRec) {
+  caseRobot = caseRec.data;
+}
+ros::NodeHandle nh;
+
+ros::Subscriber<std_msgs::Bool> subsq ("petak", petak);
+ros::Subscriber<std_msgs::Bool> subX ("kali", kali);
+ros::Subscriber<std_msgs::Bool> subO ("bulet", bulet);
+ros::Subscriber<std_msgs::Bool> subTr ("lupis", lupis);
+ros::Subscriber<std_msgs::Bool> sublsat ("lsatu", lsat);
+ros::Subscriber<std_msgs::Bool> subrsat ("rsatu", rsat);
+ros::Subscriber<std_msgs::Bool> subleft ("left", left);
+ros::Subscriber<std_msgs::Bool> subright ("right", right);
+ros::Subscriber<std_msgs::Bool> subup ("up", up);
+ros::Subscriber<std_msgs::Bool> subdown ("down", down);
+ros::Subscriber<std_msgs::Float32> sublX ("lx", lx);
+ros::Subscriber<std_msgs::Float32> sublY ("ly", ly);
+ros::Subscriber<std_msgs::Float32> subl2 ("ldua", ldua);
+ros::Subscriber<std_msgs::Float32> subr2 ("rdua", rdua);
+ros::Subscriber<std_msgs::UInt8> subsCase ("case", recCase);
+
+std_msgs::Int16 hadap;
+ros::Publisher IMU("head", &hadap);
+
+
+
+#define RADIUSMTR 0.35145 //satuan meter
+#define RADIUSBAN 0.136/2
+#define KBAN 2*M_PI*RADIUSBAN
+
+#define PWMMAX 200
+
+//MOTOR 1
+#define KP1  14
+#define KI1 8
+#define encA1 2
+#define encB1 31
+#define pwmM1 6
+#define cwM1  37
+#define ccwM1 39
+#define POSM1   45
+
+//MOTOR 2
+#define KP2 14
+#define KI2 8
+#define encA2 3
+#define encB2 23
+#define pwmM2 5
+#define cwM2  35
+#define ccwM2 33
+#define POSM2   135
+
+//MOTOR 3
+#define KP3 14
+#define KI3 8
+#define encA3 18
+#define encB3 17
+#define pwmM3 4
+#define cwM3  49
+#define ccwM3 47
+#define POSM3 225
+
+//MOTOR 4
+#define KP4 14
+#define KI4 8
+#define encA4 19
+#define encB4 41
+#define pwmM4 7
+#define cwM4  22 
+#define ccwM4 24
+#define POSM4 315
+
+#define LIM1 44 // belum fix
+#define LIM2 44
+#define LIM3 44
+#define LIM4 44
+#define LIM5 44
+#define LIM6 44
+
+
+#define KPx 2
+#define KIx 0
+
+#define KPy 2
+#define KIy 0
+
+#define KPt 1.0
+#define KIt 0
+
+#define PPR 3840
+#define RADIUSENC 0.004 //satuan meter
+#define KENCODER 2 * M_PI * 0.002
+
+volatile long int enc1, enc2, enc3, enc4, lastEnc1, lastEnc2, lastEnc3, lastEnc4;
+
+volatile float radps1, radps2, radps3, radps4;
+volatile float PID1, PID2, PID3, PID4, err1, err2, err3, err4, lastErr1, lastErr2, lastErr3, lastErr4, sum1, sum2, sum3, sum4;
+float setradPS1, setradPS2, setradPS3, setradPS4;
+
+float setX, setY, setT;
+volatile float errX, errY, errT, PIDx, PIDy, PIDt, sumX, sumY, sumT;
+volatile long int last1, last2, last3, last4;
+
+volatile unsigned long prevT, prevT1, prevT2, prevT3, prevT4;
+float absY, absX, actTheta;
+
+volatile float v1Filt, v2Filt, v3Filt, v4Filt;
+volatile float v1Prev, v2Prev, v3Prev, v4Prev;
+
+float XSmoothed, YSmoothed, TSmoothed;
+float XPrev, YPrev, TPrev;
+unsigned long waitSmooth;
+
+char bacaSer;
+bool pnu1, pnu2, pnu3, pnu4;
+float x, y;
+unsigned long curr;
+int x_temp;
+
+
+struct gy25 {
+  char buffer[50];
+  int counter;
+  float heading;
+} cmps;
+
+void kalibrasi(){
+  Serial3.begin(115200);
+
+  Serial.println("mulai kalibret");
+
+  delay(3000);
+
+  Serial3.write(0xA5);
+  Serial3.write(0x54);
+
+  delay(1000); // Jeda sebelum kalibrasi heading
+
+  // Kalibrasi Heading
+  Serial3.write(0xA5);
+  Serial3.write(0x55);
+
+  delay(100); // Jeda sebelum konfigurasi output
+
+  // Output ASCII
+  Serial3.write(0xA5);
+  Serial3.write(0x53);
+
+  delay(100); // Jeda sebentar
+
+  Serial.println("selesai kalibret");
+}
+
+void setup() {
+  Serial.begin(115200);
+//  kalibrasi();
+
+  pinMode(13, OUTPUT);
+  pinMode(encA1, INPUT);
+  pinMode(encB1, INPUT);
+
+  pinMode(encA2, INPUT);
+  pinMode(encB2, INPUT);
+
+  pinMode(encA3, INPUT);
+  pinMode(encB3, INPUT);
+
+  pinMode(encA4, INPUT);
+  pinMode(encB4, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(encA1), readEnc1, RISING);
+  attachInterrupt(digitalPinToInterrupt(encA2), readEnc2, RISING);
+  attachInterrupt(digitalPinToInterrupt(encA3), readEnc3, RISING);
+  attachInterrupt(digitalPinToInterrupt(encA4), readEnc4, RISING);
+
+  pinMode(pwmM1, OUTPUT);
+  pinMode(cwM1, OUTPUT);
+  pinMode(ccwM1, OUTPUT);
+
+  pinMode(pwmM2, OUTPUT);
+  pinMode(cwM2, OUTPUT);
+  pinMode(ccwM2, OUTPUT);
+
+  pinMode(pwmM3, OUTPUT);
+  pinMode(cwM3, OUTPUT);
+  pinMode(ccwM3, OUTPUT);
+
+  pinMode(pwmM4, OUTPUT);
+  pinMode(cwM4, OUTPUT);
+  pinMode(ccwM4, OUTPUT);
+
+  pinMode(LIM1, INPUT_PULLUP);
+  pinMode(LIM2, INPUT_PULLUP);
+  pinMode(LIM3, INPUT_PULLUP);
+  pinMode(LIM4, INPUT_PULLUP);
+  pinMode(LIM5, INPUT_PULLUP);
+  pinMode(LIM6, INPUT_PULLUP);
+
+//      nh.initNode();
+  //  nh.advertise(IMU);
+  //      nh.subscribe(subsq);
+  //      nh.subscribe(subX);
+  //      nh.subscribe(subO);
+  //      nh.subscribe(subTr);
+  //      nh.subscribe(subup);
+  //      nh.subscribe(subleft);
+  //      nh.subscribe(subright);
+  //      nh.subscribe(subdown);
+  //      nh.subscribe(sublX);
+  //      nh.subscribe(sublY);
+  //      nh.subscribe(subl2);
+  //      nh.subscribe(subr2);
+//      nh.subscribe(subsCase);
+}
+
+
+//void loop() {
+////  motor2(200);
+//  // auto
+//    nh.spinOnce();
+////  Seria/l.println(cmps.heading);
+////    Serial.print(digitalRead(LIM1)); Serial.print(" "); Serial.print(digitalRead(LIM2)); Serial.print(" "); Serial.print(digitalRead(LIM3)); Serial.print(" "); Serial.print(digitalRead(LIM4)); Serial.print(" "); Serial.print(digitalRead(LIM5)); Serial.print(" "); Serial.println(digitalRead(LIM6));
+//    Serial.print(enc1);Serial.print("\t");Serial.print(enc2);Serial.print("\t");Serial.print(enc3);Serial.print("\t");Serial.println(enc4);
+//  updateCMPS();
+//  calculatePos();
+////  Serial.print(x); Serial.print("\t");Serial.println(y);
+//  //  if (caseRobot == 0
+//
+//  //  calculatePos();
+////  if (Serial.available()) {
+////    bacaSer = Serial.read();
+////  }
+////  if (bacaSer == 'a') {
+////    if (y > -9.0) {
+////      setPos(0, -9.8, 0);
+////    }
+////    if (y < -9.0) {
+////      sum1 = 0;
+////      sum2 = 0;
+////      sum3 = 0;
+////      sum4 = 0;
+////      kinematic(0,0,0,0,0);
+////      waitMillis(1000);
+////      while (!(digitalRead(LIM1) == 0 && digitalRead(LIM2) == 0)) {
+////        calculatePos();
+////        
+////        kinematic(-2, 0, 0, 0, 1);
+////        Serial.println("nyender fence blakang");
+////        x_temp = x;
+////      }
+////      kinematic(0, 0, 0, 0, 0);
+////      waitMillis(1500);
+////      while (!(digitalRead(LIM3) == 0 && digitalRead(LIM6) == 0)) {
+////        calculatePos();
+////        kinematic(0, -3, 0, 0, 1);
+////        Serial.println("nyender fence samping");
+////      }
+////      while (bacaSer == 'a') {
+////        x = 0;
+////        y = 0;
+////        kinematic(0, 0, 0, 0, 0);
+////        if (Serial.available()) {
+////          bacaSer = Serial.read();
+////        }
+////      }
+////    }
+////
+////
+////  }
+////  if (bacaSer == 'b') {
+////    setPos(-0, 10.2, 0);
+////  }
+////  if (bacaSer == 'c') {
+////    setPos(-4, -5.2, 90);
+////  }
+////  if (bacaSer == 'd') {
+////    setPos(-4, -5.2, 0);
+////  }
+////  if (bacaSer == 'e') {
+////    setPos(0, 0, 0);
+////  }
+//
+//  if (caseRobot == 8) {
+//    if (y < 11) {
+//      setPos(0, 11.8, 178.9);
+//    }
+//    if (y > 11 ) {
+//      sum1 = 0;
+//      sum2 = 0;
+//      sum3 = 0;
+//      sum4 = 0;
+//      kinematic(0,0,0,0,0);
+//      waitMillis(1000);
+//      while (!(digitalRead(LIM1) == 0 && digitalRead(LIM2) == 0)) {
+//        calculatePos();
+//        nh.spinOnce();
+//        kinematic(-2.5, 0, 0, 0, 1);
+//        Serial.println("nyender fence blakang");
+//        x_temp = x;
+//      }
+//      kinematic(0, 0, 0, 0, 0);
+//      waitMillis(1500);
+//      while (!(digitalRead(LIM3) == 0 && digitalRead(LIM6) == 0)) {
+//        calculatePos();
+//        nh.spinOnce();
+//        kinematic(0, -3, 0, 0, 1);
+//        Serial.println("nyender fence samping");
+//      }
+//      sum1 = 0;
+//      sum2 = 0;
+//      sum3 = 0;
+//      sum4 = 0;
+//      XSmoothed = x;
+//      YSmoothed = y;
+//      XPrev = x;
+//      YPrev = y;
+//      while (caseRobot == 8) {
+//        kinematic(0, 0, 0, 0, 0);
+//        nh.spinOnce();
+//      }
+//    }
+//
+//
+//  }
+//  if (caseRobot == 9) {
+//    setPos(0, 0, 0);
+//  }
+//  if (caseRobot == 1){
+//    if (y > -9.2) {
+//      setPos(0, -10.0, 0);
+//    }
+//    if (y < -9.2) {
+//      sum1 = 0;
+//      sum2 = 0;
+//      sum3 = 0;
+//      sum4 = 0;
+//      kinematic(0,0,0,0,0);
+//      waitMillis(1000);
+//      while (!(digitalRead(LIM4) == 0 && digitalRead(LIM5) == 0)) {
+//        calculatePos();
+//        nh.spinOnce();
+//        kinematic(2.5, 0, 0, 0, 1);
+//        Serial.println("nyender fence blakang");
+//        x_temp = x;
+//      }
+//      kinematic(0, 0, 0, 0, 0);
+//      waitMillis(1500);
+//      while (!(digitalRead(LIM3) == 0 && digitalRead(LIM6) == 0)) {
+//        calculatePos();
+//        nh.spinOnce();
+//        kinematic(0, -3, 0, 0, 1);
+//        Serial.println("nyender fence samping");
+//      }
+//      sum1 = 0;
+//      sum2 = 0;
+//      sum3 = 0;
+//      sum4 = 0;
+//      XSmoothed = x;
+//      YSmoothed = y;
+//      XPrev = x;
+//      YPrev = y;
+//      while (caseRobot == 1) {
+//        calculatePos();
+//        kinematic(0, 0, 0, 0, 0);
+//        nh.spinOnce();
+//      }
+//    }
+//  }
+//  if (caseRobot == 3) {
+//    setPos(-1.5, -6, 90);
+//  }
+//  if(caseRobot == 99){
+//    setPos(-9,0,0);
+//  }
+//}
+
+void loop(){
+  setradPS4 = 2*M_PI * 4;
+  PID();
+//  Serial.println(v1Filt);
+  Serial.print(setradPS4); Serial.print(" "); Serial.println(v4Filt);
+//Serial.print(enc1);Serial.print("\t");Serial.print(enc2);Serial.print("\t");Serial.print(enc3);Serial.print("\t");Serial.println(enc4);
+//  motor3(100);
+}
---
+++ Arduino/elephantBase/lib.ino
@@ -0,0 +1,237 @@
+void readEnc1() {
+  digitalRead(encB1) == 1 ? enc1++ : enc1--;
+}
+
+void readEnc2() {
+  digitalRead(encB2) == 1 ? enc2++ : enc2--;
+}
+
+void readEnc3() {
+  digitalRead(encB3) == 1 ? enc3++ : enc3--;
+}
+
+void readEnc4() {
+  digitalRead(encB4) == 1 ? enc4++ : enc4--;
+}
+
+float toRad(float degree) {
+  return degree * M_PI / 180;
+}
+
+void smoothing() {
+  //smoothing
+  XSmoothed = (setX * 0.005) + (XPrev * 0.995);
+  XPrev = XSmoothed;
+  YSmoothed = (setY * 0.005) + (YPrev * 0.995);
+  YPrev = YSmoothed;
+  TSmoothed = (setT * 0.005) + (TPrev * 0.995);
+  TPrev = TSmoothed;
+}
+
+
+
+void updateCMPS() {
+  char tmp; // Variabel temporary
+
+  while (Serial3.available()) {
+    tmp = Serial3.read();
+    cmps.buffer[cmps.counter++] = tmp;
+    if (tmp == '\n') { // Langkah 1
+      cmps.buffer[cmps.counter] = 0; // Karakter terminator
+      cmps.heading = atof(strtok(cmps.buffer + 5, ",")); // Langkah 2-4
+      cmps.counter = 0;
+    }
+  }
+}
+
+void waitMillis(int millisec) {
+  curr = millis();
+  do{
+    nh.spinOnce();
+    calculatePos();
+  }
+  while (!(millis() - curr > millisec));
+}
+
+void motor1 (int pwm) {
+  pwm = max(-PWMMAX, min(pwm, PWMMAX));
+  analogWrite(pwmM1, fabs(pwm));
+
+  if (pwm > 0) {
+    digitalWrite(cwM1, 1);
+    digitalWrite(ccwM1, 0);
+  }
+  else if (pwm < 0) {
+    digitalWrite(cwM1, 0);
+    digitalWrite(ccwM1, 1);
+  }
+  else {
+    digitalWrite(cwM1, 0);
+    digitalWrite(ccwM1, 0);
+  }
+}
+
+void motor2 (int pwm) {
+  pwm = max(-PWMMAX, min(pwm, PWMMAX));
+  analogWrite(pwmM2, fabs(pwm));
+
+  if (pwm > 0) {
+    digitalWrite(cwM2, 1);
+    digitalWrite(ccwM2, 0);
+  }
+  else if (pwm < 0) {
+    digitalWrite(cwM2, 0);
+    digitalWrite(ccwM2, 1);
+  }
+  else {
+    digitalWrite(cwM2, 0);
+    digitalWrite(ccwM2, 0);
+  }
+}
+
+void motor3 (int pwm) {
+  pwm = max(-PWMMAX, min(pwm, PWMMAX));
+  analogWrite(pwmM3, fabs(pwm));
+
+  if (pwm > 0) {
+    digitalWrite(cwM3, 1);
+    digitalWrite(ccwM3, 0);
+  }
+  else if (pwm < 0) {
+    digitalWrite(cwM3, 0);
+    digitalWrite(ccwM3, 1);
+  }
+  else {
+    digitalWrite(cwM3, 0);
+    digitalWrite(ccwM3, 0);
+  }
+}
+
+void motor4 (int pwm) {
+  pwm = max(-PWMMAX, min(pwm, PWMMAX));
+  analogWrite(pwmM4, fabs(pwm));
+
+  if (pwm > 0) {
+    digitalWrite(cwM4, 1);
+    digitalWrite(ccwM4, 0);
+  }
+  else if (pwm < 0) {
+    digitalWrite(cwM4, 0);
+    digitalWrite(ccwM4, 1);
+  }
+  else {
+    digitalWrite(cwM4, 0);
+    digitalWrite(ccwM4, 0);
+  }
+}
+
+void kinematic(float vX, float vY, float theta, float heading, bool closedloop) {
+  setradPS1 = (-sin(toRad(POSM1 + heading)) * vX + cos(toRad(POSM1 + heading)) * vY + theta * RADIUSMTR) / RADIUSBAN;
+  setradPS2 = (-sin(toRad(POSM2 + heading)) * vX + cos(toRad(POSM2 + heading)) * vY + theta * RADIUSMTR) / RADIUSBAN;
+  setradPS3 = (-sin(toRad(POSM3 + heading)) * vX + cos(toRad(POSM3 + heading)) * vY + theta * RADIUSMTR) / RADIUSBAN;
+  setradPS4 = (-sin(toRad(POSM4 + heading)) * vX + cos(toRad(POSM4 + heading)) * vY + theta * RADIUSMTR) / RADIUSBAN;
+  if(closedloop == 1){
+    PID();
+  }
+  else{
+    motor1(setradPS1*13);
+    motor2(setradPS2*10);
+    motor3(setradPS3*5);
+    motor4(setradPS4*13);    
+  }
+
+}
+
+void setPos(float POSX, float POSY, float HADAP) {
+  setX = POSX;
+  setY = POSY;
+  setT = HADAP;
+  calculatePos();
+  kinematic(PIDx, PIDy, PIDt, cmps.heading,1);
+
+}
+
+void calculatePos() {
+  float v1 = (enc1 - last1) * KBAN / (PPR);
+  float v2 = (enc2 - last2) * KBAN / (PPR);
+  float v3 = (enc3 - last3) * KBAN / (PPR);
+  float v4 = (enc4 - last4) * KBAN / (PPR);
+
+  last1 = enc1;
+  last2 = enc2;
+  last3 = enc3;
+  last4 = enc4;
+
+  float Vx = -sin(toRad(cmps.heading + POSM1)) * v1 + -sin(toRad(cmps.heading + POSM2)) * v2 + -sin(toRad(cmps.heading + POSM3)) * v3 + -sin(toRad(cmps.heading + POSM4)) * v4;
+  float Vy = cos(toRad(cmps.heading + POSM1)) * v1 + cos(toRad(cmps.heading + POSM2)) * v2 + cos(toRad(cmps.heading + POSM3)) * v3 + cos(toRad(cmps.heading + POSM4)) * v4;
+
+  x += Vx;
+  y += Vy;
+}
+
+void PID() {
+  smoothing();
+  volatile unsigned long currT = micros();
+  float deltaT = ((float) (currT - prevT)) / 1.0e6;
+  prevT = currT;
+
+  radps1 = ((enc1 - lastEnc1) * 2 * M_PI / PPR) / deltaT;
+  radps2 = ((enc2 - lastEnc2) * 2 * M_PI / PPR) / deltaT;
+  radps3 = ((enc3 - lastEnc3) * 2 * M_PI / PPR) / deltaT;
+  radps4 = ((enc4 - lastEnc4) * 2 * M_PI / PPR) / deltaT;
+  lastEnc1 = enc1;
+  lastEnc2 = enc2;
+  lastEnc3 = enc3;
+  lastEnc4 = enc4;
+
+  v1Filt = 0.854 * v1Filt + 0.0728 * radps1 + 0.0728 * v1Prev;
+  v1Prev = radps1;
+  v2Filt = 0.854 * v2Filt + 0.0728 * radps2 + 0.0728 * v2Prev;
+  v2Prev = radps2;
+  v3Filt = 0.854 * v3Filt + 0.0728 * radps3 + 0.0728 * v3Prev;
+  v3Prev = radps3;
+  v4Filt = 0.854 * v4Filt + 0.0728 * radps4 + 0.0728 * v4Prev;
+  v4Prev = radps4;
+
+  //PID KECEPATAN
+  err1 = setradPS1 - v1Filt;
+  sum1 += err1;
+  PID1 = KP1 * err1 + KI1 * sum1 * deltaT;
+
+  err2 = setradPS2 - v2Filt;
+  sum2 += err2;
+  PID2 = KP2 * err2 + KI2 * sum2 * deltaT;
+
+  err3 = setradPS3 - v3Filt;
+  sum3 += err3;
+  PID3 = KP3 * err3 + KI3 * sum3 * deltaT;
+
+  err4 = setradPS4 - v4Filt;
+  sum4 += err4;
+  PID4 = KP4 * err4 + KI4 * sum4 * deltaT;
+
+  //PID Posisi
+  errX = XSmoothed - x;
+  sumX += errX;
+  PIDx = KPx * errX + KIx * sumX * deltaT;
+//  PIDx = fmaxf(-7.3, fminf(PIDx,7.3));
+
+  errY = YSmoothed - y;
+  sumY += errY;
+  PIDy = KPy * errY + KIy * sumY * deltaT;
+//  PIDy = fmaxf(-7.3, fminf(PIDy, 7.3));
+
+  
+  errT = TSmoothed - cmps.heading;
+  if(errT > 180 || errT < -180){
+    errT = cmps.heading - TSmoothed;
+  }
+  sumT += errT;
+  PIDt = KPt * errT + KIt * sumT * deltaT;
+  PIDt = fmaxf(-3.7, fminf(PIDt, 3.7));
+
+  motor1(PID1);
+  motor2(PID2);
+  motor3(PID3);
+  motor4(PID4);
+}
